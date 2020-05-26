fb0ff370c9999232d55d87251e669b40dfb866eb
Avanish474 <60430980+Avanish474@users.noreply.github.com>
2020-05-26 15:54:13 +0530

    Update fashionmodel.py

--- fashionmodel.py
+++ fashionmodel.py
@@ -1,36 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[51]:
-
-
-import tensorflow as tf
-from tensorflow import keras
 import os
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
-n=2
-previous = 2
-new = 2**n
-accuracy_file=open('/tf/tensorflow-tutorials/accuracy.txt','rt')
-while((int(accuracy_file.read(5))*100)<80):
-    
+
+
+while((float(open('/tf/tensorflow-tutorials/accuracy.txt','rt').read(5))*100)<=89):
+    n=int(open('/tf/tensorflow-tutorials/n.txt','rt').read())
     f=open('/tf/tensorflow-tutorials/classifications.py','rt')
     data=f.read()   
-      data=data.replace('n={0}'.format(previous),'n={0}'.format(new))            
+    data=data.replace('n={0}'.format(n),'n={0}'.format(n*2))            
     f.close()
     f=open('/tf/tensorflow-tutorials/classifications.py','wt')
-   
     f.write(data)
     f.close()
     os.system('python3 /tf/tensorflow-tutorials/classifications.py')
-    accuracy_file=file
-    previous=new
-    n=2*n
-    new=2**n
-    
-
 
 # In[ ]:
 
