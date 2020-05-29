39bc5f0c5f43391d837792b1b28c244e154c6a02
Avanish474 <60430980+Avanish474@users.noreply.github.com>
2020-05-29 12:27:46 +0530

    Update classifications.py

--- classifications.py
+++ classifications.py
@@ -1,76 +1,6 @@
 
-#!/usr/bin/env python
-# coding: utf-8
-
-# ##### Copyright 2018 The TensorFlow Authors.
-
-# In[19]:
 
 
-#@title Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-# In[20]:
-
-
-#@title MIT License
-#
-# Copyright (c) 2017 François Chollet
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
-
-# # Basic classification: Classify images of clothing
-
-# <table class="tfo-notebook-buttons" align="left">
-#   <td>
-#     <a target="_blank" href="https://www.tensorflow.org/tutorials/keras/classification"><img src="https://www.tensorflow.org/images/tf_logo_32px.png" />View on TensorFlow.org</a>
-#   </td>
-#   <td>
-#     <a target="_blank" href="https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/keras/classification.ipynb"><img src="https://www.tensorflow.org/images/colab_logo_32px.png" />Run in Google Colab</a>
-#   </td>
-#   <td>
-#     <a target="_blank" href="https://github.com/tensorflow/docs/blob/master/site/en/tutorials/keras/classification.ipynb"><img src="https://www.tensorflow.org/images/GitHub-Mark-32px.png" />View source on GitHub</a>
-#   </td>
-#   <td>
-#     <a href="https://storage.googleapis.com/tensorflow_docs/docs/site/en/tutorials/keras/classification.ipynb"><img src="https://www.tensorflow.org/images/download_logo_32px.png" />Download notebook</a>
-#   </td>
-# </table>
-
-# This guide trains a neural network model to classify images of clothing, like sneakers and shirts. It's okay if you don't understand all the details; this is a fast-paced overview of a complete TensorFlow program with the details explained as you go.
-# 
-# This guide uses [tf.keras](https://www.tensorflow.org/guide/keras), a high-level API to build and train models in TensorFlow.
-
-# In[21]:
-
-
-import pickle
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
@@ -85,16 +15,7 @@ print(tf.__version__)
 # ## Import the Fashion MNIST dataset
 
 # This guide uses the [Fashion MNIST](https://github.com/zalandoresearch/fashion-mnist) dataset which contains 70,000 grayscale images in 10 categories. The images show individual articles of clothing at low resolution (28 by 28 pixels), as seen here:
-# 
-# <table>
-#   <tr><td>
-#     <img src="https://tensorflow.org/images/fashion-mnist-sprite.png"
-#          alt="Fashion MNIST sprite"  width="600">
-#   </td></tr>
-#   <tr><td align="center">
-#     <b>Figure 1.</b> <a href="https://github.com/zalandoresearch/fashion-mnist">Fashion-MNIST samples</a> (by Zalando, MIT License).<br/>&nbsp;
-#   </td></tr>
-# </table>
+
 # 
 # Fashion MNIST is intended as a drop-in replacement for the classic [MNIST](http://yann.lecun.com/exdb/mnist/) dataset—often used as the "Hello, World" of machine learning programs for computer vision. The MNIST dataset contains images of handwritten digits (0, 1, 2, etc.) in a format identical to that of the articles of clothing you'll use here.
 # 
@@ -102,7 +23,7 @@ print(tf.__version__)
 # 
 # Here, 60,000 images are used to train the network and 10,000 images to evaluate how accurately the network learned to classify images. You can access the Fashion MNIST directly from TensorFlow. Import and load the Fashion MNIST data directly from TensorFlow:
 
-# In[22]:
+
 
 
 fashion_mnist = keras.datasets.fashion_mnist
@@ -117,58 +38,7 @@ fashion_mnist = keras.datasets.fashion_mnist
 # 
 # The images are 28x28 NumPy arrays, with pixel values ranging from 0 to 255. The *labels* are an array of integers, ranging from 0 to 9. These correspond to the *class* of clothing the image represents:
 # 
-# <table>
-#   <tr>
-#     <th>Label</th>
-#     <th>Class</th>
-#   </tr>
-#   <tr>
-#     <td>0</td>
-#     <td>T-shirt/top</td>
-#   </tr>
-#   <tr>
-#     <td>1</td>
-#     <td>Trouser</td>
-#   </tr>
-#     <tr>
-#     <td>2</td>
-#     <td>Pullover</td>
-#   </tr>
-#     <tr>
-#     <td>3</td>
-#     <td>Dress</td>
-#   </tr>
-#     <tr>
-#     <td>4</td>
-#     <td>Coat</td>
-#   </tr>
-#     <tr>
-#     <td>5</td>
-#     <td>Sandal</td>
-#   </tr>
-#     <tr>
-#     <td>6</td>
-#     <td>Shirt</td>
-#   </tr>
-#     <tr>
-#     <td>7</td>
-#     <td>Sneaker</td>
-#   </tr>
-#     <tr>
-#     <td>8</td>
-#     <td>Bag</td>
-#   </tr>
-#     <tr>
-#     <td>9</td>
-#     <td>Ankle boot</td>
-#   </tr>
-# </table>
-# 
 # Each image is mapped to a single label. Since the *class names* are not included with the dataset, store them here to use later when plotting the images:
-
-# In[23]:
-
-
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
@@ -177,39 +47,23 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 # 
 # Let's explore the format of the dataset before training the model. The following shows there are 60,000 images in the training set, with each image represented as 28 x 28 pixels:
 
-# In[24]:
 
 
 train_images.shape
-
-
 # Likewise, there are 60,000 labels in the training set:
-
-# In[25]:
-
-
 len(train_labels)
 
 
 # Each label is an integer between 0 and 9:
 
-# In[26]:
-
-
 train_labels
 
 
 # There are 10,000 images in the test set. Again, each image is represented as 28 x 28 pixels:
-
-# In[27]:
-
-
 test_images.shape
 
 
 # And the test set contains 10,000 images labels:
-
-# In[28]:
 
 
 len(test_labels)
@@ -218,8 +72,6 @@ len(test_labels)
 # ## Preprocess the data
 # 
 # The data must be preprocessed before training the network. If you inspect the first image in the training set, you will see that the pixel values fall in the range of 0 to 255:
-
-# In[29]:
 
 
 plt.figure()
